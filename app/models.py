b75e17a26a45e20d7a49bd8cb6826210cfece9ed
Alexey Kulichevskiy <alexey.kulichevskiy+alexchevsky@gmail.com>
2023-07-22 13:48:40 +0200

    added repr to models

--- app/models.py
+++ app/models.py
@@ -8,6 +8,9 @@ class Student(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.user.email}"
+
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
@@ -16,6 +19,9 @@ class Course(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.title}"
+
 
 class Class(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -23,9 +29,15 @@ class Class(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.course.title} Class starting on {self.start_date}"
+
 
 class Enrollment(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Class, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Enrollment {self.id} for {self.student.user.email} in {self.course.course.title}"
